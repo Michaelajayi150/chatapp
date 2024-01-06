80055d7c194da841413ef2916a5437f2948b7f33
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-06 23:45:33 +0100

    Confirmed connection to database

--- server/utils/dbconnect.js
+++ server/utils/dbconnect.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
+let username = process.env.DB_USER
+let password = process.env.DB_PASSWORD
+let dbCluster = process.env.DB_STRING
+let dbName = process.env.DB_NAME
 async function dbConn() {
-    const dbString = "mongodb://localhost:27017/Chatapp"
-    await mongoose.connect(dbString)
+    const dbURI = `mongodb+srv://${username}:${password}@${dbCluster}/${dbName}?retryWrites=true&w=majority`;
+    await mongoose.connect(dbURI)
 }
 
 module.exports = dbConn

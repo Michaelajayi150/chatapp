1d5935ded8449ab5c80c573fbcce1447500d7c7f
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-07 18:41:48 +0100

    Patch to address client side routing issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,12 +12,12 @@ function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route exact path="/" element={<WelcomePage />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/chatroom/:id" element={<Chat />} />
-        <Route exact path="/chatrooms" element={<Chatrooms />} />
-        <Route exact path="*" element={<NotFound/>} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chatroom/:id" element={<Chat />} />
+        <Route path="/chatrooms" element={<Chatrooms />} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
   );

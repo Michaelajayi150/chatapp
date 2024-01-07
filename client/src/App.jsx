c5105c144312905ffdfadda542b5a18dd7696564
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-07 19:12:24 +0100

    Patch to address client side routing issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import WelcomePage from './components/WelcomePage';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Chat from './components/Chat';
 import Chatrooms from './components/Chatrooms';
+import WelcomePage from './components/WelcomePage';
 import NotFound from './components/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/chatroom/:id" element={<Chat />} />
-        <Route path="/chatrooms" element={<Chatrooms />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/chatroom/:id" component={Chat} />
+        <Route exact path="/chatrooms" component={Chatrooms} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }

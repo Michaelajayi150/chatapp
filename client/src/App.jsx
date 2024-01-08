a696dc437ade87e6ff93c472946b26313d80f08d
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-08 10:10:57 +0100

    Added patch to address client side routing issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.css'
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Chat from './components/Chat';
